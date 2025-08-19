6da9142d2d4fbc00f78b7ffaa8826902ca94b692
William01br <williamstcorreia123@gmail.com>
2025-08-18 21:05:05 -0300

    feat: add title component

---
+++ src/components/title/index.tsx
@@ -0,0 +1,15 @@
+import { Text, TextProps } from "react-native";
+import { styles } from "./styles";
+import React from "react";
+
+type Props = TextProps & {
+  children: React.ReactNode;
+};
+
+export default function Title({ children, ...rest }: Props) {
+  return (
+    <Text style={styles.title} {...rest}>
+      {children}
+    </Text>
+  );
+}
---
+++ src/components/title/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 40,
+    color: "#000000",
+  },
+});
